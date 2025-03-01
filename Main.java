0aa31f03727668c14b804cd8650fd13f2a2964d9
Ethan Luong <129918674+e10hog@users.noreply.github.com>
2025-03-01 23:22:44 +0000

    created method

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 // When you are ready to have your work checked, change the value below from N to Y
-// ReadyForSubmission=N
-// YOUR NAME:
+// ReadyForSubmission=Y
+// YOUR NAME: ETHAN LUONG
 // COLLABORATORS:
-// DATE:  
+// DATE: 3/1/25
 
 public class Main
 {
@@ -10,9 +10,14 @@ public class Main
 	{
 		System.out.println("My thoughts on the article are:");
 		// Part 2: Invoking/calling method
-		
+		printThoughts();
 	}
 	
 	// Part 1: Creating method definition
-	
+	public static void printThoughts() {
+		String first = "Pedestrians are more likely to be hurt than the people in the car, so it is should prioritize the safety of pedestrians.\n";
+		String second = "The environment of the car since that would determine what paths or maneuvers it can take to protect the pedestrians/passengers, but also reduce the amount of damage to the environment. Also, if the car could recognize the ages and status of the pedestrians, it could prioritize saving children and elders over adults.\n";
+		String third = "Not only should the company decide this algorithm, but it should consider the inputs of the public and the government to agree on how should the car act in these scenarios.";
+		System.out.println(first + second + third);
+	}
 }
